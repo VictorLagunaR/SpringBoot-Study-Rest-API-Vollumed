7274f9e666cdc3a7f43d3bb18a59457e1a8ee66c
VictorLagunaR <vitor.v.l.rlaguna@gmail.com>
2024-01-02 18:41:01 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,24 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.Paciente.DadosCadastroPaciente;
+import med.voll.api.Paciente.Paciente;
+import med.voll.api.Paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+     @Autowired
+     private PacienteRepository repository;
+     @PostMapping
+     @Transactional
+     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+         repository.save(new Paciente(dados));
+     }
+}
